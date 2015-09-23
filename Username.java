57ad0ea1ad54d403ed2e88f815eaccd1ca2bee4a
Jerry tran <jerrytran3011@gmail.com>
2015-09-22 22:35:51 -0400

    Assignment2.java

--- Username.java
+++ Username.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
+
 public class Username {
   public static void main(String[] args){
     String correctUsername = "CSC200";
     String correctPassword = "Password";
     String username ="", password ="";
     //Main method
-    System.out.println("Enter your Usernam and Password");
+    System.out.println("Enter your Username and Password");
     Scanner keyboard = new Scanner(System.in);
 
     username = keyboard.next();
     password = keyboard.next();
     
     if(username.equals(correctUsername)&& password.equals(correctPassword)){
-     system.out.print("Welcome " + Username);
+      System.out.print("Welcome " + username);
     } else { 
-     system.out.print("Wrong username and password");
+      System.out.print("Wrong username and password");
      }
     }
  }
